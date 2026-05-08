d17e341ea162cbd132d9ee610ff9242d36122e76
Davi-UnB <daviseverianofreitas@gmail.com>
2026-05-08 00:29:49 -0300

    docs(projeto): define escopo do sistema hospitalar e responsabilidades

--- src/entidade/Paciente.java
+++ src/entidade/Paciente.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Paciente {
     // Dados permanentes
     private String nome,
-                   cpf;
+            cpf;
     private char sexo;
     private int idade;
     private boolean temCadastro;
@@ -43,16 +43,28 @@ public class Paciente {
         return cpf;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScorePrioridade() {
+        return scorePrioridade;
+    }
+
     public void setScorePrioridade(int scorePrioridade) {
         this.scorePrioridade = scorePrioridade;
+    }
+
+    public LocalDateTime getChegada() {
+        return chegada;
     }
 
     public void setChegada(LocalDateTime chegada) {
@@ -65,5 +77,53 @@ public class Paciente {
 
     public void setCondicoesAtuais(List<Condicao> condicoesAtuais) {
         this.condicoesAtuais = condicoesAtuais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean temCadastro() {
+        return temCadastro;
+    }
+
+    public void setTemCadastro(boolean temCadastro) {
+        this.temCadastro = temCadastro;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public List<Condicao> getHistoricoCondicoes() {
+        return historicoCondicoes;
+    }
+
+    public void setHistoricoCondicoes(List<Condicao> historicoCondicoes) {
+        this.historicoCondicoes = historicoCondicoes;
     }
 }
